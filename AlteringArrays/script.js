abdeebbff9437dcf572580e813d7bba6e73433c2
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-08-10 16:23:10 -0300

    Update script.js

--- AlteringArrays/script.js
+++ AlteringArrays/script.js
@@ -1,5 +1,16 @@
-function oi(){
-    alert("OLA");
+function cadastrar_funcionarios(){
+     var funcionarios = [];
+     while(true){
+         aux = prompt("Digite o nome do proximo funcionario ou 0 para sair");
+         if(aux == 0)
+             break;
+         else
+             funcionarios.unshift(aux);
+     }
+
+     for (i in funcionarios){
+         document.write("<br>o nome é: ", funcionarios[i]);
+     }
 }
 
-oi();
+cadastrar_funcionarios()
