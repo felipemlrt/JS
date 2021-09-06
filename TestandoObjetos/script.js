1159f0ac22db0c4eccfe7c4bd623adee0a77df0a
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-09-06 08:28:54 -0300

    Update script.js

--- TestandoObjetos/script.js
+++ TestandoObjetos/script.js
@@ -15,7 +15,7 @@ class Pessoa{
 }
 
 function enviar_dados(){
-    var nome_input = document.getElementById("aaa").value
+    var nome_input = document.getElementById("input_nome").value
     //receber o dado "felipe" parte 3
     var email_input = document.getElementById("bbb").value
     var idade_input = document.getElementById("ccc").value
