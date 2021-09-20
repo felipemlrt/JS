89872fcbf2e2ba0066d8ba46926f6b35d6d0d452
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-09-20 14:22:29 -0300

    Update script.js

--- Correcao/script.js
+++ Correcao/script.js
@@ -1,13 +1,11 @@
+//Quais afirmacoes a seguir sao verdadeiras?
 //axpressão (a != 3 || b < 10 || c == 5) é falso.
 //bxpressão (d > 8 && c == 3 || a >= 10) é verdadeiro.
 //cxpressão (d == 12 && a != 10) é falso.
 //dexpressão (c == 4 || d <=6) && (a >= 5 && b !=9) || ( ! (a < 5) ) é falso.
-//e.expressão (a == 3 || b > 10 || d == 8 ) é verdadeiro.
-true
-true
-false
-true
-false
+//e.expressão (a == 3 || b > 10 || d == 8 ) é verdadeiro.[
+
+//verificando:
 a =5, b=7, c=3,d=9
 document.write((a != 3 || b < 10 || c == 5),"<br>")
 document.write((d > 8 && c == 3 || a >= 10),"<br>")
