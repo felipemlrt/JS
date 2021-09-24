f3438ab0ce0187dd24067e85016f94ed389b0fc0
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-09-24 15:31:45 -0300

    Update script.js

--- Correcao/script.js
+++ Correcao/script.js
@@ -16,5 +16,7 @@ document.write((d > 8 && c == 3 || a >= 10),"<br>")
 
 //c) expressão: (d == 12 && a != 10) é falso.
 document.write(!(d == 12 && a != 10),"<br>")
+
+//d) expressão: (c == 4 || d <=6) && (a >= 5 && b !=9) || ( ! (a < 5) ) é falso.
 document.write((c == 4 || d <=6) && (a >= 5 && b !=9) || ( ! (a < 5) ),"<br>")
 document.write((a == 3 || b > 10 || d == 8 ),"<br>")
