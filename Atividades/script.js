faf5bb7dc0be039b5d735e90b9607da51f5fc8be
Felipe Soares Muylaert Barroso <felipemlrt99@gmail.com>
2021-10-25 12:46:48 -0300

    Update script.js

--- Atividades/script.js
+++ Atividades/script.js
@@ -1,18 +1,18 @@
 //1) Crie um código que gere a sequencia de fibonacci
 //1 1 2 3 5 8 13 21 34 exemplo de sequencia, no caso com 7 repetições
-// function fibonacci(limite_de_repeticoes) {
-//   var anterior_1 = 1, anterior_2 = 1;
-//   document.write(anterior_1, " ", anterior_2, " ");
-//   for (i = 0; i < limite_de_repeticoes; i++) {
-//     atual = anterior_1 + anterior_2;
-//     document.write(atual, " ");
-//     anterior_2 = anterior_1;
-//     anterior_1 = atual;
-//   }
-// }
+ function fibonacci(limite_de_repeticoes) {
+   var anterior_1 = 1, anterior_2 = 1;
+   document.write(anterior_1, " ", anterior_2, " ");
+   for (i = 0; i < limite_de_repeticoes; i++) {
+     atual = anterior_1 + anterior_2;
+     document.write(atual, " ");
+     anterior_2 = anterior_1;
+     anterior_1 = atual;
+   }
+ }
 
-// repeticoes = prompt("Quantas repeticoes da sequencia?")
-// fibonacci(repeticoes)
+ repeticoes = prompt("Quantas repeticoes da sequencia?")
+ fibonacci(repeticoes)
 
 //2) Faça uma função que verifique se os 3 valores medidos estao a menos de 10% da media dentre eles
 
